dff3234bb9bf4f95c5875953a60afbff2d45843c
Alberto Nao <alnao@alnao.it>
2023-05-22 20:37:57 +0200

    upload bug fix

--- ManageFile/UploadFileToRestAPI/upload.py
+++ ManageFile/UploadFileToRestAPI/upload.py
@@ -1,24 +1,28 @@
 #Uploader Filer to Rest API
+# in windows to generate exe
+# > pip install PyInstaller 
+# > pyInstaller  --onefile upload.py
+
 import requests
-#from requests.auth import HTTPBasicAuth
 import json
 from pathlib import Path
 import os
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-#from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
+import jwt
+
+C_jwt_signature='password'
+C_api_endpoint="https://aaaaaaaaaaaaaa.execute-api.eu-west-1.amazonaws.com/dev"
 
 def get_API_endpoint():
-    url="https://aaaaaaaaaaaaaaaaaa.execute-api.eu-west-1.amazonaws.com/dev"
+    url=C_api_endpoint
     return url
 
-#JWT TOKEN
-def create_token_jwt():
-    #message = {'iss': 'CLIENT_ID','exp': (datetime.now(timezone.utc) + timedelta(hours=23))}
-    #print (message)
-    #encoded_jwt = jwt.encode(message, 'password', algorithm='HS256')
-    encoded_jwt = 'token'
+def create_token_jwt(utente):
+    message = {'iss': utente,'exp': (datetime.now(timezone.utc) + timedelta(hours=1))}
+    encoded_jwt = jwt.encode(message, C_jwt_signature , algorithm='HS256')
     return  encoded_jwt
 
 def choose_file():
@@ -28,10 +32,10 @@ def choose_file():
     return file_path
 
 def create_header(encoded_jwt):
-    file_ids = ''
     headers={ "authorization": ""+ encoded_jwt
              ,"content-type": "application/x-www-form-urlencoded;charset=ISO-8859-15"
              ,"accept": "application/json"}
+    return headers
 
 def get_file(file_path, username):
     f = open(file_path, 'rb') #file={"file": (file_path, f) }
@@ -47,13 +51,14 @@ def get_username():
     user=''
     if os.getenv('username'):
         user=os.getenv('username')
-    return user
+    return user.lower()
 
 def post_file(url, data, headers):
-    print("CALL API" + url)
+    print("CALL API with data")
+    print(data)
     resp = requests.post(url,data=data,files=data, headers=headers )#, files=files
-    print (resp.text)
     print ( "status code " + str(resp.status_code) )
+    print (resp.text)
     return resp
 
 def check_response(resp,window_title):
@@ -66,13 +71,15 @@ def check_response(resp,window_title):
         print ("Failure")
         if "User: anonymous is not authorized to perform: execute-api" in resp.text :
             messagebox.showwarning(title=window_title, message="Impossibile collegarsi al server, verificare che la VPN sia attiva",)
+        #elseif '{"message":"Unauthorized"}' in rest.txt:
+        #    messagebox.showwarning(title=window_title, message="Impossibile caricare il file",)
         else:
-            messagebox.showerror(title=window_title, message=resp.text,)
+            messagebox.showerror(title=window_title, message="Impossibile caricicare il file:" + resp.text,)
 
 if __name__ == '__main__':
     url = get_API_endpoint()
-    encoded_jwt = create_token_jwt()
     user = get_username()
+    encoded_jwt = create_token_jwt(user)
     file_path = choose_file()
     data = get_file(file_path, user)
     headers = create_header(encoded_jwt)
