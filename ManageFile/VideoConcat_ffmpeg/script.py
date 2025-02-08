c17649fed663a469fe19650a81417285a56c05aa
Alberto Nao <alnao@alnao.it>
2025-02-08 18:05:35 +0100

    Concat video mp4 & mkv

--- ManageFile/VideoConcat_ffmpeg/script.py
+++ ManageFile/VideoConcat_ffmpeg/script.py
@@ -13,12 +13,19 @@ from os.path import isfile, join
 #import ffmpeg                           #   pip install ffmpeg  --break-system-packages
 #see https://github.com/kkroening/ffmpeg-python
 
-C_MP4=".mkv"
+C_MP4=".mp4"
+C_MKV=".mkv"
 C_OGV=".ogv"
 
 # SEE https://www.educative.io/answers/how-to-run-a-python-script-in-linux
 # https://stackoverflow.com/questions/11295917/how-to-select-a-directory-and-store-the-location-using-tkinter-in-python
 
+
+def selectExtension(fileName):
+    if fileName.endswith(C_MP4) or fileName.endswith(C_MKV):
+        return True
+    else:
+        return False
 
 def selectFolder():
     root = Tk()
@@ -28,13 +35,15 @@ def selectFolder():
     return folder_selected
 
 def filesInFolder(folder):
-    only_files = [f for f in listdir(folder) if isfile(join(folder, f)) and f.endswith(C_MP4)  ]
+    only_files = [f for f in listdir(folder) if isfile(join(folder, f)) and selectExtension(f)  ]
     return only_files
 
 def concatVideo(folder,files):
     list_ts=[]
     #ffmpeg -i aaaaaaa.ogv -vcodec libx264 "B_Vivian_tetteMutande.mp4"
     for file in files:
+        print("--------------------")
+        print("Inizio a processare il video " , file)
         com="ffmpeg -i "+folder+"/"+ file + " -c copy -bsf:v h264_mp4toannexb -f mpegts " +folder+ "/fileIntermediate"+str(len(list_ts))+ ".ts"
         list_ts.append(folder+ "/fileIntermediate"+str(len(list_ts))+ ".ts")
         print(com)
@@ -45,6 +54,9 @@ def concatVideo(folder,files):
         #print(com)
         #os.system(com)
     #ffmpeg -i "concat:fileIntermediate3.ts|fileIntermediate1.ts|fileIntermediate2.ts" -c copy -bsf:a aac_adtstoasc mergedVideo.mp4
+    print("--------------------")
+    print("FInito il ciclo, inizio ad unire i video")
+
     list_ts_str="|".join( list_ts )
     com="ffmpeg -i \"concat:"+list_ts_str+"\" -c copy -bsf:a aac_adtstoasc "+folder+"/out.mp4"
     print(com)
